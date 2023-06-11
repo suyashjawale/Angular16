c86a701b6eef33db43779122e297585624424508
Suyash Jawale <suyashjawale245@gmail.com>
2023-06-11 20:27:10 +0530

    Files added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+// import { HomeComponent } from './components/home/home.component';
+// import { CartComponent } from './components/cart/cart.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:'home',
+        // component:HomeComponent,
+        loadComponent: () => import('./components/home/home.component').then(mod => mod.HomeComponent)
+    },
+    {
+        path:'cart',
+        // component:CartComponent
+        loadComponent: () => import('./components/cart/cart.component').then(mod => mod.CartComponent)
+    }
+];
---
+++ src/app/components/myorders/myorders.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-myorders',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './myorders.component.html',
+  styleUrls: ['./myorders.component.css']
+})
+export class MyordersComponent {
+
+}
