71db302f247d1f92d7f79efdb2e698d741814aaf
Suyash Jawale <suyashjawale245@gmail.com>
2023-06-12 20:39:52 +0530

    Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent,
+        loadComponent: () => import('./components/home/home.component').then(mod => mod.HomeComponent),
     },
     {
         path: 'category',
